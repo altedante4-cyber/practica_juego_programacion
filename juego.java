d65e532cefcfaac568e787de326db8ab442657ba
michael aguilar <altedante4@gmail.com>
2025-12-22 12:30:07 +0100

    anadiendo funciones

--- juego.java
+++ juego.java
@@ -2,13 +2,36 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class juego {
+//variable globales definidas
+//alamacena la palabra secreta
+    static  String palabras[] = {"pirlo", "cafes", "messi"};
+//continee el numero de intentos que ha consumido el jugador sera como maximo de 6
+    static int contador = 0;  // contador para  terminar la partida 
+// contiene el numero de letras adivinadas por el jugador
+    static String numLetrasAdivinadas[] = new String [10];
+//objeto de tipo scanner 
     static Scanner sc = new Scanner(System.in);
+//importamos random para elegir la palabra
+static   Random leter = new Random();
 
+    //elegir palabra del array de plabras
+static   int elegir_palabra = leter.nextInt(palabras.length);
+//palabra introducida por el jugador 
+static  String usuario ;
+
+static String palabra_jugar  = generaPalabra(palabras, elegir_palabra);
+
+
+
+    
     public static void main(String[] args) {
-        Random leter = new Random();
         char opcion;
-        String palabras[] = {"pirlo", "cafes", "messi"};
-		char[] caracteresEspeciales = {
+
+        //palabra seleccionado para jugar la partida
+
+        
+
+        char[] caracteresEspeciales = {
     				'!', '@', '#', '$', '%', '^', '&', '*',
     				'(', ')', '-', '_', '=', '+',
     				'{', '}', '[', ']', '|',
@@ -28,17 +51,14 @@ public class juego {
             opcion = sc.nextLine().charAt(0);
             
             if (opcion == 'n') {
-                // Elegimos palabra aleatoria
-                int elegir_palabra = leter.nextInt(palabras.length);
-                String palabra_jugar = palabras[elegir_palabra]; //aqui elegimos la palabra a jugar 
-                int contador = 0;  // contador para  terminar la partida 
-
 				boolean encontrado_consonantes = false;
 				boolean encontrado_palabra_no_admitida = false; //booleano que utilizamos para saber la ultima posicion del string usuario si tiene 'q' , 'x' ,'w' 
 				boolean contador_vocales_seguidas = false;
+
+                //INICIAMOS LA PARTIDA CON 6 INTENTOS
+
 				while (contador < 6 ) {
                     
-                    String usuario ;
 					int contador_interno = 0 ;
 					boolean salir_bucle_errores = false ; 
 
@@ -191,26 +211,25 @@ for (int i = 0; i < usuario.length(); i++) {
 		System.out.println("Error tiene 3 vocales seguidassssssssss ");
 	}
 
-
-	if(encontrado_consonantes){
+    if(encontrado_consonantes){
 		System.out.print("Error estas intentando introducir tres consonantes seguidas");
 
 	}
-	if(encontrado_palabra_no_admitida){
+	 if(encontrado_palabra_no_admitida){
 		 System.out.print("Erro la palabra no puede terminar ('q','x','w')");
 	}
 
 
-	if(tiene_caracteres_especiales){
+	 if(tiene_caracteres_especiales){
 		System.out.print("Error , vuelve a intentarlo tiene caracteres especiales tu palabara");
 		contador_interno += 1 ; 
 	}
 
 	//condicion de salida de bucle por que ya introdujo 6 veces el mismo error 
-    if (contador_interno >= 6) {
+    else if (haTerminadoJuego(contador_interno)) {
         salir_bucle_errores = true;
     }
-	if(tiene_caracteres_especiales){
+	else if(tiene_caracteres_especiales){
 		contador_interno += 1 ; 
 		System.out.print("Tiene caracteres especiales vuelve a intentarlo o tiene espacios ");
 	}
@@ -222,7 +241,65 @@ for (int i = 0; i < usuario.length(); i++) {
      	   			System.out.println("Demasiados intentos fallidos de entrada.");
         			break; // Esto rompe el bucle 'while (contador < 6)'
     			}
-                    // Creamos un array para guardar el resultado visual de este intento
+                   
+   //// ** aqui va kla  funcion
+                    char resultador [] =compruebaLetrasAcertadas(usuario);
+
+                    //imprimos el resultado 
+                    System.out.print("Resultado: ");
+                    for (int i = 0; i < resultador.length; i++) {
+                        System.out.print(resultador[i]);
+                    }
+                    System.out.println(); // salto de linea 
+
+					//condicion si el usuario aserto  
+                    if(haGanadoJugador(usuario)){
+                         System.out.print("Ha Ganado la partida");
+                         break;
+                    }
+                    
+                    contador++;
+                }
+                 if(haTerminadoJuego(contador)){
+                System.out.println("Se ha terminado tu partida");
+            }
+            }
+           
+        } while (opcion != 's'); 
+	
+	}
+
+//devuelve un booleano inicando si ha ganado el jugador al acertar la palabra secreta
+ public static boolean haGanadoJugador(String user  ){
+
+    if(user.equals(palabra_jugar)){
+        return true ;
+    }
+
+    return false ; 
+
+
+ }
+
+ // haTerminadoJuego indica si ha terminado el juego al consumir el jugador los 6 intententos 
+
+ public static boolean haTerminadoJuego(int cont){
+     
+     if(cont == 6 ){
+        return true ;
+     }
+
+     return false ;
+ }
+
+//compurbea LetrasAcertadas se le pasa un string de 5 legras independientemente de si el jugador ha introducido mayuscula o minusculas 
+//devuelve  si ha acertado la letra y su posicion (letra maysucula ) is ha acertado la letra, pero no su posicion(letra minuscula) o no ha
+//acertado lal letra(*) es decir devuelve un string de 5 caracteres 
+
+
+public static char [] compruebaLetrasAcertadas(String user ){
+     
+     // Creamos un array para guardar el resultado visual de este intento
                     char resultado[] = new char[palabra_jugar.length()];
 
                     // Recorremos la palabra del usuario letra por letra
@@ -255,24 +332,17 @@ for (int i = 0; i < usuario.length(); i++) {
                             }
                         }
                     }
-
-                    //imprimos el resultado 
-                    System.out.print("Resultado: ");
-                    for (int i = 0; i < resultado.length; i++) {
-                        System.out.print(resultado[i]);
-                    }
-                    System.out.println(); // salto de linea 
-
-					//condicion si el usuario aserto  
-                    if (usuario.equals(palabra_jugar)) {
-                        System.out.println("¡Ganaste!");
-                        break;
-                    }
-                    
-                    contador++;
+     
+return resultado;
                 }
-            }
-        } while (opcion != 's'); 
-	
-	}
+
+
+public static String generaPalabra( String a[],int elegir_palabra ){
+     
+    String elegir = a[elegir_palabra];
+
+    return elegir;
+     
+}
+
 }
